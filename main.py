f36b83a84372ea2b412e81b2b569b19d3efa2296
naldotga23 <naldotga2016@hotmail.com>
2026-07-26 19:28:08 -0400

    Update main.py
    Adiciona endpoint /process

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from fastapi.responses import JSONResponse
 
 app = FastAPI(title="AutoClip Backend")
+
 
 @app.get("/")
 def home():
     return {"message": "AutoClip Backend Online"}
 
+
 @app.get("/health")
 def health():
     return {"status": "ok"}
 
+
+@app.post("/process")
+def process():
+    return JSONResponse(
+        status_code=status.HTTP_202_ACCEPTED,
+        content={
+            "status": "accepted",
+            "message": "Processamento iniciado com sucesso."
+        },
+    )
